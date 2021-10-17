d15dd18e35b37cd8c33ad7a3c30437c1d0bc0a54
lichingchester <chester.li@onepage.com.hk>
2021-10-17 15:09:52 +0800

    convert doc to nuxtjs

---
+++ docs/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header class="p-5">
+      <div class="container">
+        <h1 class="text-3xl">NoIE Document</h1>
+      </div>
+    </header>
+
+    <div class="grid">
+      
+    </div>
+
+    <nuxt/>
+  </div>
+</template>
